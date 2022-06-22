bf9e7b01fa55c742064724a91fccfd06d5c10d93
donaldk12345 <fotsoyvesdonald@gmail.com>
2022-06-22 19:02:29 +0200

    controller paypal en construction

--- src/Controller/PaypalController.php
+++ src/Controller/PaypalController.php
@@ -2,17 +2,24 @@
 
 namespace App\Controller;
 
+use App\Services\Panier\ShoppingCart;
+use App\Services\Paypal\Paypal;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class PaypalController extends AbstractController
 {
-    #[Route('/paypal', name: 'app_paypal')]
-    public function index(): Response
-    {
-        return $this->render('paypal/index.html.twig', [
-            'controller_name' => 'PaypalController',
-        ]);
+   
+
+    public function createPaiement(ShoppingCart $shoppingCart,Paypal $paypal){
+
+        $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
+        
+       
+
     }
+  
+
+
 }
